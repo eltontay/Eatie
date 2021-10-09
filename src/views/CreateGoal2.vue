c23ded7c6cc6a57697092b264b42b6876416f5d9
Elton <57287843+eltontay@users.noreply.github.com>
2021-10-09 15:41:17 +0800

    small edits

--- src/views/CreateGoal2.vue
+++ src/views/CreateGoal2.vue
@@ -3,14 +3,14 @@
     <h2>Step (2/4)</h2>
     <h3>What is your height?</h3>
     <div id="flex-container">
-    <div id="flex-child">
-      <img src="@/assets/height.png" alt="" class="" />
-    </div>
-    <form id="flex-child">
-      <label for="height">My height (cm) is</label>
-      <input type="text" id="height" />
-      <button @click="Step2()">Next</button>
-    </form>
+      <div id="flex-child">
+        <img src="@/assets/height.png" alt="" class="" />
+      </div>
+      <form id="flex-child">
+        <label for="height">My height (cm) is</label>
+        <input type="text" id="height" />
+        <button @click="Step2()">Next</button>
+      </form>
     </div>
   </div>
 </template>
--- src/views/CreateGoal4.vue
+++ src/views/CreateGoal4.vue
@@ -5,19 +5,19 @@
       <h3>What is your daily activity level?</h3>
       <div>
         <button @click="Step4()">
-                    <img class = "activity1" src="@/assets/activity1.png" alt=""> 
+          <img class="activity1" src="@/assets/activity1.png" alt="" />
           <h3>I am seated most of the time</h3>
         </button>
         <button @click="Step4()">
-          <img class = "activity2" src="@/assets/activity2.png" alt=""> 
+          <img class="activity2" src="@/assets/activity2.png" alt="" />
           <h3>I am standing half of the time</h3>
         </button>
         <button @click="Step4()">
-          <img class = "activity3" src="@/assets/activity3.png" alt=""> 
+          <img class="activity3" src="@/assets/activity3.png" alt="" />
           <h3>I am walking half of the time</h3>
         </button>
         <button @click="Step4()">
-          <img class = "activity4" src="@/assets/activity4.png" alt=""> 
+          <img class="activity4" src="@/assets/activity4.png" alt="" />
           <h3>I am constantly on the move</h3>
         </button>
       </div>

e0665b6acaf6ffa5d8f051f3a4f8847622e5029a
Elton Tay <eltontay1997@gmail.com>
2021-10-09 10:26:30 +0800

    merging

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
-  <SignUp/> <br><br>
-  <SignIn/>
+  <v-app>
+    <navigation :color="color" :flat="flat" />
+    <v-main class="pt-0">
+      <SignUp />
+      <SignIn />
+      <Home />
+    </v-main>
+    <v-scale-transition>
+      <v-btn
+        fab
+        v-show="fab"
+        v-scroll="onScroll"
+        dark
+        fixed
+        bottom
+        right
+        color="secondary"
+        @click="toTop"
+      >
+        <v-icon>mdi-arrow-up</v-icon>
+      </v-btn>
+    </v-scale-transition>
+    <foote />
+  </v-app>
 </template>
 
 <script>
-import SignUp from './components/SignUp.vue'
-import SignIn from './components/SignIn.vue'
+import SignUp from "./components/SignUp.vue";
+import SignIn from "./components/SignIn.vue";
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     SignUp,
-    SignIn
-  }
-}
+    SignIn,
+    Home,
+  },
+};
 </script>
 
 <style>

65c91aae0543f1a97207290d0c951dea9cebd2ef
pngqunshen <pngqunshen@gmail.com>
2021-10-18 16:52:56 +0800

    Even more graphs and gives nutrient recommendation

--- src/components/MyProgressDaily.vue
+++ src/components/MyProgressDaily.vue
@@ -3,7 +3,7 @@
       <h2>Daily Summary</h2>
       <div id="dailySummary">
         <div id="halfDailySummary1">
-            <h3>{{calorieGoalRecommendation}}</h3>
+            <h3>{{calorieGoalRecommendation}}</h3><br>
             <div id="dailySummary">
                 <div id="halfDailySummary1">
                     <div style="font-size: 120%;">Calories Goal: <strong>{{goalValue}}</strong></div>
@@ -20,9 +20,15 @@
             <!-- add link here -->
         </div>
         <div id="halfDailySummary2">
-            <pie-chart :colors="['rgb(7, 4, 155)', 'rgb(5, 52, 155)', 'rgb(36, 105, 255)']"  height=80%
+            <pie-chart :colors="['rgb(7, 4, 155)', 'rgb(5, 52, 155)', 'rgb(36, 105, 255)']"  height=300px
                 :data = "dailyNutrientPieData"></pie-chart>
-            <h3>{{nutrientBreakdownRecommendation}}</h3>
+            <h3 v-if="carbohydratesLower">You should increase your consumption of carbohydrates!</h3>
+            <h3 v-if="carbohydratesUpper">You should reduce your consumption of carbohydrates!</h3>
+            <h3 v-if="fatLower">You should increase your consumption of fat!</h3>
+            <h3 v-if="fatUpper">You should reduce your consumption of fat!</h3>
+            <h3 v-if="proteinLower">You should increase your consumption of protein!</h3>
+            <h3 v-if="proteinUpper">You should reduce your consumption of protein!</h3>
+            <h3 v-if="nutrientRecommendation">Your nutrient breakdown is within recommendation!</h3>
         </div>
       </div>
   </div>
@@ -32,29 +38,57 @@
 export default {
     data(){
         return{
-            goalValue: 2300, //change value
-            consumedValue: 2000, //change value
-            dailyNutrientPieData: {'Carbohydrates': 4, 'Fat': 1, 'Protein': 2} //change value
+            goalValue: 2500, //change value
+            dailyNutrientPieData: {'Carbohydrates': 1500, 'Fat': 600, 'Protein': 200}, //change value
         }
     }, computed:{
+        consumedValue(){
+            return this.dailyNutrientPieData['Carbohydrates'] + 
+            this.dailyNutrientPieData['Fat'] + 
+            this.dailyNutrientPieData['Protein']
+        },
         remainingValue(){
             return this.goalValue - this.consumedValue
         },
-        dailyCaloriePieData(){
-            return {'consumed': this.consumedValue, 'remaining': this.remainingValue}
-        },
         percentageCalorie(){
             return Math.round(this.consumedValue/this.goalValue*100)
+        },
+        dailyCaloriePieData(){
+            return {'consumed': this.consumedValue, 'remaining': this.remainingValue}
         },
         calorieGoalRecommendation(){
             return this.remainingValue < 100
             ? "You are nearing your recommended Calorie goal. Try not to eat too much!"
             : "Great job! You are still within your recommended Calorie intake!"
         },
-        nutrientBreakdownRecommendation(){
-            return this.remainingValue < 100 // change conditions and message
-            ? "Your nutrient breakdown is within recommendation!"
-            : "Your nutrient breakdown is within recommendation!"
+        idealNutrientBreakdown() {
+            return {
+                'Carbohydrates': [0.45*this.consumedValue, 0.65*this.consumedValue], 
+                'Fat': [0.25*this.consumedValue,0.35*this.consumedValue], 
+                'Protein': [0.1*this.consumedValue,0.35*this.consumedValue]
+                }
+        },
+        carbohydratesLower(){
+            return this.dailyNutrientPieData['Carbohydrates'] < this.idealNutrientBreakdown['Carbohydrates'][0]
+        },
+        carbohydratesUpper(){
+            return this.dailyNutrientPieData['Carbohydrates'] > this.idealNutrientBreakdown['Carbohydrates'][1]
+        },
+        fatLower(){
+            return this.dailyNutrientPieData['Fat'] < this.idealNutrientBreakdown['Fat'][0]
+        },
+        fatUpper(){
+            return this.dailyNutrientPieData['Fat'] > this.idealNutrientBreakdown['Fat'][1]
+        },
+        proteinLower(){
+            return this.dailyNutrientPieData['Protein'] < this.idealNutrientBreakdown['Protein'][0]
+        },
+        proteinUpper(){
+            return this.dailyNutrientPieData['Protein'] > this.idealNutrientBreakdown['Protein'][1]
+        },
+        nutrientRecommendation(){
+            return !this.carbohydratesLower && !this.carbohydratesUpper 
+                && !this.fatLower && !this.fatUpper && !this.proteinLower && !this.proteinUpper
         }
     }
 }
--- src/components/MyProgressJourney.vue
+++ src/components/MyProgressJourney.vue
@@ -3,10 +3,12 @@
       <h2>My Journey</h2>
       <div id="myJourney">
         <div id="halfMyJourney">
-            <h3>Insert graph here</h3>
+            <line-chart v-bind:min=minWeight v-bind:max=maxWeight :data = "weeklyWeightLineData"></line-chart>
+            <h3>My Weight Journey</h3>
         </div>
         <div id="halfMyJourney">
-            <h3>Insert graph here</h3>
+            <column-chart :data = "weeklyNutrientDistribution"></column-chart>
+            <h3>Weekly Nutrient Distribution</h3>
         </div>
       </div>
       <h3>Diagnosis</h3>
@@ -18,8 +20,40 @@
 export default {
     data(){
         return{
-            myJourneyDiagnosis: "Diagnosis Message"
+            myJourneyDiagnosis: "Diagnosis Message",
+            weeklyWeightLineData: {"Week1": 90, 'Week2': 89, 'Week3': 86, 'Week4': 88, 'Week5': 85}, //change value
+            weeklyNutrientDistribution: {"carbohydrates": 10000, "fat": 3500, "protein": 2000} //change value
         }
+    }, computed:{
+        maxWeight(){
+            let maxValue=0
+            for(const val of Object.entries(this.weeklyWeightLineData)) {
+                  if(val[1] > maxValue){
+                      maxValue = val[1];
+                  }
+            }
+            return maxValue + 5;
+        },
+        minWeight(){
+            let minValue=1000
+            for(const val of Object.entries(this.weeklyWeightLineData)) {
+                  if(val[1] < minValue){
+                      minValue = val[1];
+                  }
+            }
+            return minValue - 5;
+        },
+        totalConsumedCalorie(){
+            return this.weeklyNutrientDistribution["carbohydrates"] + this.weeklyNutrientDistribution["fat"] +
+                this.weeklyNutrientDistribution["protein"]
+        },
+        idealNutrientBreakdown() {
+            return {
+                'Carbohydrates': [0.45*this.totalConsumedCalorie, 0.65*this.totalConsumedCalorie], 
+                'Fat': [0.25*this.totalConsumedCalorie,0.35*this.totalConsumedCalorie], 
+                'Protein': [0.1*this.totalConsumedCalorie,0.35*this.totalConsumedCalorie]
+                }
+        },
     }
 }
 </script>

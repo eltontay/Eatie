405d7f0251508b92ce855c7e6c13f2d14c31f517
pngqunshen <pngqunshen@gmail.com>
2021-10-18 15:10:52 +0800

    Updated graphs

--- src/components/MyProgressDaily.vue
+++ src/components/MyProgressDaily.vue
@@ -2,16 +2,16 @@
   <div class="container">
       <h2>Daily Summary</h2>
       <div id="dailySummary">
-        <div id="halfDailySummary">
+        <div id="halfDailySummary1">
             <h3>{{calorieGoalRecommendation}}</h3>
             <div id="dailySummary">
-                <div id="halfDailySummary">
-                    <div>Calories Goal: {{goalValue}}</div>
-                    <div>Calories Consumed: {{consumedValue}}</div>
-                    <div>Calories Remaining: {{remainingValue}}</div>
+                <div id="halfDailySummary1">
+                    <div style="font-size: 120%;">Calories Goal: <strong>{{goalValue}}</strong></div>
+                    <div style="font-size: 120%;">Calories Consumed: <strong style="color: green">{{consumedValue}}</strong></div>
+                    <div style="font-size: 120%;">Calories Remaining: <strong style="color: red">{{remainingValue}}</strong></div>
                 </div>
-                <div id="halfDailySummary">
-                    <pie-chart :colors="['rgb(17, 207, 255)', 'rgb(1, 40, 49)']" donut=true height=100px 
+                <div id="halfDailySummary2">
+                    <pie-chart :colors="['rgb(17, 207, 255)', 'rgb(1, 40, 49)']" donut=true height=50% 
                         legend=hide :data = "dailyCaloriePieData"></pie-chart>
                     <h3>{{percentageCalorie}}%</h3>
                 </div>
@@ -19,8 +19,8 @@
             <h3>Just had a meal? Add it to My Journal now!</h3>
             <!-- add link here -->
         </div>
-        <div id="halfDailySummary">
-            <pie-chart :colors="['rgb(7, 4, 155)', 'rgb(5, 52, 155)', 'rgb(36, 105, 255)']" 
+        <div id="halfDailySummary2">
+            <pie-chart :colors="['rgb(7, 4, 155)', 'rgb(5, 52, 155)', 'rgb(36, 105, 255)']"  height=80%
                 :data = "dailyNutrientPieData"></pie-chart>
             <h3>{{nutrientBreakdownRecommendation}}</h3>
         </div>
@@ -64,10 +64,15 @@ export default {
 #dailySummary {
     display: flex;
 }
-#halfDailySummary {
+#halfDailySummary1 {
     width: 40%;
     margin: 0% 5% 0% 5%;
     text-align: left;
+}
+#halfDailySummary2 {
+    width: 40%;
+    margin: 0% 5% 0% 5%;
+    text-align: center;
 }
 #dailyNutrientPieChart {
     width: 5px;

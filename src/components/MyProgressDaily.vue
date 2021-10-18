a90c9cf0783329e803b2a7bcdb31409142ec7b38
pngqunshen <pngqunshen@gmail.com>
2021-10-18 14:36:50 +0800

    Included graphs in My Progress

--- src/components/MyProgressDaily.vue
+++ src/components/MyProgressDaily.vue
@@ -11,14 +11,18 @@
                     <div>Calories Remaining: {{remainingValue}}</div>
                 </div>
                 <div id="halfDailySummary">
-                    Insert graph here
+                    <pie-chart :colors="['rgb(17, 207, 255)', 'rgb(1, 40, 49)']" donut=true height=100px 
+                        legend=hide :data = "dailyCaloriePieData"></pie-chart>
+                    <h3>{{percentageCalorie}}%</h3>
                 </div>
             </div>
             <h3>Just had a meal? Add it to My Journal now!</h3>
             <!-- add link here -->
         </div>
         <div id="halfDailySummary">
-            <h3>Insert graph here</h3>
+            <pie-chart :colors="['rgb(7, 4, 155)', 'rgb(5, 52, 155)', 'rgb(36, 105, 255)']" 
+                :data = "dailyNutrientPieData"></pie-chart>
+            <h3>{{nutrientBreakdownRecommendation}}</h3>
         </div>
       </div>
   </div>
@@ -28,17 +32,29 @@
 export default {
     data(){
         return{
-            goalValue: 0,
-            consumedValue: 0
+            goalValue: 2300, //change value
+            consumedValue: 2000, //change value
+            dailyNutrientPieData: {'Carbohydrates': 4, 'Fat': 1, 'Protein': 2} //change value
         }
     }, computed:{
         remainingValue(){
             return this.goalValue - this.consumedValue
         },
+        dailyCaloriePieData(){
+            return {'consumed': this.consumedValue, 'remaining': this.remainingValue}
+        },
+        percentageCalorie(){
+            return Math.round(this.consumedValue/this.goalValue*100)
+        },
         calorieGoalRecommendation(){
             return this.remainingValue < 100
             ? "You are nearing your recommended Calorie goal. Try not to eat too much!"
             : "Great job! You are still within your recommended Calorie intake!"
+        },
+        nutrientBreakdownRecommendation(){
+            return this.remainingValue < 100 // change conditions and message
+            ? "Your nutrient breakdown is within recommendation!"
+            : "Your nutrient breakdown is within recommendation!"
         }
     }
 }
@@ -53,4 +69,10 @@ export default {
     margin: 0% 5% 0% 5%;
     text-align: left;
 }
+#dailyNutrientPieChart {
+    width: 5px;
+    background-color: rgb(1, 40, 49)
+}
+
+
 </style>

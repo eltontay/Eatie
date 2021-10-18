5f2aa4a38157c92186010429ba807b1d756489a5
Elton <57287843+eltontay@users.noreply.github.com>
2021-10-19 00:47:31 +0800

    stupid auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import {initializeApp} from 'firebase/app';
+import firebase from 'firebase/compat/app';
 
 //leaving my own firebase here for testing
 let auth = require('./dbconfig.json');
@@ -8,11 +8,11 @@ const firebaseConfig = {
   projectId: auth['projectId'],
   storageBucket: auth['storageBucket'],
   messagingSenderId: auth['messagingSenderId'],
-  appId: auth['appId']
+  appId: auth['appId'],
 };
 
 // Initialize Firebase
-const firebaseapp = initializeApp(firebaseConfig);
-console.log("test");
-console.log(firebaseapp);
-export default firebaseapp;
+firebase.initializeApp(firebaseConfig);
+console.log('test');
+console.log(firebase);
+export default firebase;

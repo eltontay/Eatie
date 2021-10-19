de92b3a7432e1a090c9280dbf7f48791433fde4d
pngqunshen <pngqunshen@gmail.com>
2021-10-20 00:02:36 +0800

    Fix the firebase auth issue

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,6 +1,6 @@
 <template>
-  <form id="signInPortal">
     <div id="firebaseui-auth-container"></div>
+  <!-- <form id="signInPortal">
     <label for="login_username">Username:</label>
     <input
       type="text"
@@ -21,52 +21,53 @@
     <button id="authUser" type="button" v-on:click="authenticateUser()">
       Sign In
     </button>
-  </form>
+  </form> -->
 </template>
 
 <script>
+import firebase from '@/uifire.js'
 import * as firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css';
-import firebaseApp from '../firebase.js';
-import { getFirestore } from 'firebase/firestore';
-import { getDoc, doc } from 'firebase/firestore';
+// import firebaseApp from '../firebase.js';
+// import { getFirestore } from 'firebase/firestore';
+// import { getDoc, doc } from 'firebase/firestore';
 
-const db = getFirestore(firebaseApp);
+// const db = getFirestore(firebaseApp);
 
 export default {
   mounted() {
     var ui = firebaseui.auth.AuthUI.getInstance();
     if (!ui) {
       // Initialize the FirebaseUI Widget using Firebase.
-      ui = new firebaseui.auth.AuthUI(firebaseApp.auth());
+      ui = new firebaseui.auth.AuthUI(firebase.auth());
     }
     ui.start('#firebaseui-auth-container', {
       signInSuccessUrl: '/',
       signInOptions: [
-        firebaseui.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebaseui.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
       ],
     });
   },
-  methods: {
-    async authenticateUser() {
-      var a = String(document.getElementById('login_username').value);
-      var b = String(document.getElementById('login_password').value);
-      console.log(a);
+  // methods: {
+  //   async authenticateUser() {
+  //     var a = String(document.getElementById('login_username').value);
+  //     var b = String(document.getElementById('login_password').value);
+  //     console.log(a);
 
-      const acct = await getDoc(doc(db, 'acct_detail', a));
-      console.log('working...');
+  //     const acct = await getDoc(doc(db, 'acct_detail', a));
+  //     console.log('working...');
 
-      if (acct.exists()) {
-        console.log('User exists');
-        if (b == acct.data().password) {
-          console.log('User is authenticated');
-        }
-      } else {
-        console.log('No User Found');
-      }
-    },
-  },
+  //     if (acct.exists()) {
+  //       console.log('User exists');
+  //       if (b == acct.data().password) {
+  //         console.log('User is authenticated');
+  //       }
+  //     } else {
+  //       console.log('No User Found');
+  //     }
+  //   },
+  // },
 };
 </script>
 

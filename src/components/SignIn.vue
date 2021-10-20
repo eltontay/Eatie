7932d207fa72d5f5a1f7e4bb4286b7b3d1a36140
aczy <angela.chen@flex-solver.com.sg>
2021-10-20 12:20:05 +0800

    Removing old code for SignIn

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,38 +1,11 @@
 <template>
     <div id="firebaseui-auth-container"></div>
-  <!-- <form id="signInPortal">
-    <label for="login_username">Username:</label>
-    <input
-      type="text"
-      id="login_username"
-      required="True,"
-      placeholder="Enter your username"
-    />
-    <br /><br />
-    <label for="login_password">Password:</label>
-    <input
-      type="text"
-      id="login_password"
-      required="True,"
-      placeholder="Enter your password"
-    />
-    <br /><br />
-
-    <button id="authUser" type="button" v-on:click="authenticateUser()">
-      Sign In
-    </button>
-  </form> -->
 </template>
 
 <script>
 import firebase from '@/uifire.js'
 import * as firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css';
-// import firebaseApp from '../firebase.js';
-// import { getFirestore } from 'firebase/firestore';
-// import { getDoc, doc } from 'firebase/firestore';
-
-// const db = getFirestore(firebaseApp);
 
 export default {
   mounted() {
@@ -48,26 +21,7 @@ export default {
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
       ],
     });
-  },
-  // methods: {
-  //   async authenticateUser() {
-  //     var a = String(document.getElementById('login_username').value);
-  //     var b = String(document.getElementById('login_password').value);
-  //     console.log(a);
-
-  //     const acct = await getDoc(doc(db, 'acct_detail', a));
-  //     console.log('working...');
-
-  //     if (acct.exists()) {
-  //       console.log('User exists');
-  //       if (b == acct.data().password) {
-  //         console.log('User is authenticated');
-  //       }
-  //     } else {
-  //       console.log('No User Found');
-  //     }
-  //   },
-  // },
+  }
 };
 </script>
 

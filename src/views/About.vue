a5dfb0fc23482c3475aab1cb29aaead2572eee56
Justin Chu <59039021+justinnjuulius@users.noreply.github.com>
2021-10-23 12:41:48 +0800

    Add to AddFood component and About view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,69 @@
 <template>
-<div class="container">
-    <h2>About</h2>
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe fuga porro esse, maiores eligendi dolorem similique magnam rerum maxime totam corrupti reprehenderit, eius a consequuntur perspiciatis quis! Esse, veritatis explicabo?
+
+<div style="text-align:center;" v-if="user">
+
+    <NavBar/>
+
+    <h3>About</h3>
+    <h2>Welcome to Eatie!</h2>
+    <h3>How does it work? 
+        <br> “Eatie” is a food journal web application that promotes weight-loss through pictorial food logs, calorie tracking and detailed nutritional breakdowns. 
+            With Eatie’s health profile algorithm, users will be able to get personalised calorie targets according to their unique profiles. Eatie also has an extensive 
+            food database that can assist users to get a more accurate nutritional breakdown of their food intake. This would allow users to take better control of their goals 
+            and understand which nutrition they are lacking. Eatie adopts a pictorial food journal to make journaling fun and easy. 
+        <br> <br>
+    </h3>
+    <LogOut /> <br><br>    
+
 </div>
+  
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue'
+import LogOut from '@/components/LogOut.vue'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
 export default {
-  name: 'About',
-}
+    name:"About" ,
+
+    components:{
+    NavBar,
+    LogOut
+
+    },
+
+    data() {
+    return {
+      user:false,        
+        }
+    },
+
+    mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+    if (user) {
+      this.user = user;      
+      }
+  })
+
+    },
+
+}   
 </script>
 
-<style>
+<style scoped>
+
+.about{
+   
+    text-align: left;
+    border: 2px solid black;
+    box-shadow: 3px 3px grey 
+
+}
+
+
 
 </style>
+

1fcfffde3381de55d53a7146a855c173048a2f3f
Justin Chu <59039021+justinnjuulius@users.noreply.github.com>
2021-10-27 17:53:50 +0800

    addfood

--- src/components/addfood.vue
+++ src/components/addfood.vue
@@ -1,22 +1,13 @@
 <template>
   <div class ="container">
             <form id = "myform" >
-                <h2>Add Meal and it's respective Macronutrients</h2>    
+                <h2>Add Meal name and date</h2>    
                 <div class = "formli">
                     <label for="Meal">Meal Name: </label>
                     <input type="text" id = "Meal" required="" placeholder="Enter Meal Name"> <br><br>
 
-                    <label for="Calories">Calories: </label>
-                    <input type="text" id = "Calories" required="" placeholder="Enter Calories"> <br><br>
-
-                    <label for="Protein">Protein: </label>
-                    <input type="number" id = "Protein" required="" placeholder="Enter Protein"> <br><br>
-
-                    <label for="Fat">Fat: </label>
-                    <input type="number" id = "Fat" required="" placeholder="Enter Fat"> <br><br>
-
-                    <label for="Carbohydrates">Carbohydrates: </label>
-                    <input type="number" id = "Carbohydrates" required="" placeholder="Enter Carbohydrates"> <br><br>
+                    <label for="Date">Date: </label>
+                    <input type="text" id = "Date" required="" placeholder="Enter Date"> <br><br>
 
                     <div class = "save">
                     <button id = "savebutton"  type="button" v-on:click="savetofs()"> Save </button><br><br>
@@ -36,21 +27,14 @@ const db = getFirestore(firebaseApp);
 export default {
     methods: {    
         async saveFoodtoFs(){
-            var a  = document.getElementById("Meal").value
-            var b  = document.getElementById("Calories").value
-            var c  = document.getElementById("Protein").value
-            var d =  document.getElementById("Fat").value
-            var e =  document.getElementById("Carbohydrates").value
+            var a  = searchFood("Meal")
 
             alert(" Saving your data for Meal : " + a) 
 
             try{
                 const docRef = await setDoc(doc(db, "Portfolio", a),{
                     Meal: a,
-                    Calories :b,
-                    Protein: c,
-                    Fat : d,
-                    Carbohydrates : e
+                    Date :b,
                 })
                 console.log(docRef)
                 document.getElementById('myform').reset();

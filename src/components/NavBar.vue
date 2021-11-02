4b6ef8cd02f88866e2557e74d151ad172e95d28c
Elton <57287843+eltontay@users.noreply.github.com>
2021-11-03 01:01:54 +0800

    added about and food calculator in sign in page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,41 +1,42 @@
 <template>
-  <div id = 'nav' v-if="user">
+  <div id="nav" v-if="user">
     <router-link to="/">Home</router-link> |
     <router-link to="/foodCalculator">Food Calculator</router-link> |
     <router-link to="/myJournal">My Journal</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/mygoals">My Goals</router-link> | 
-    <router-link to="/myProgress">My Progress</router-link> | 
+    <router-link to="/mygoals">My Goals</router-link> |
+    <router-link to="/myProgress">My Progress</router-link> |
     <router-link to="/Profile">Profile</router-link>
   </div>
-  <div id = 'nav' v-if="!user">
-      <router-link to="/SignIn">Sign In</router-link>
+  <div id="nav" v-if="!user">
+    <router-link to="/about">About</router-link> |
+    <router-link to="/foodCalculator">Food Calculator</router-link> |
+    <router-link to="/SignIn">Sign In</router-link>
   </div>
 </template>
 
 <script>
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export default {
-    name: 'NavBar',
+  name: 'NavBar',
 
-    data() {
-        return {
-            user: false,
-        }
-    },
+  data() {
+    return {
+      user: false,
+    };
+  },
 
-    mounted() {
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                this.user = user;
-            }
-        })
-    }
-}
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  },
+};
 </script>
-
 
 <style scoped>
 #nav {
@@ -45,11 +46,10 @@ export default {
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50
+  color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-    color: #42b983
+  color: #42b983;
 }
-
 </style>

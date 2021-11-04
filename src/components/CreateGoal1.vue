98d0162b0aba1cea5fbce9ab8d5f3c14c9630010
unhappyraccoon <xiaozuoling@outlook.com>
2021-11-04 14:23:24 +0800

    "test on ui"

--- src/components/CreateGoal1.vue
+++ src/components/CreateGoal1.vue
@@ -1,29 +1,22 @@
- <template>
- <div class="container">
-   <h2>Step (1/4)</h2>
-   <div>
-     <h3>What is your gender?</h3>
-     <div>
-       <button @click="gender1()" id="boy" value="boy">
-         <img src="@/assets/boy.png" alt="" />
-       </button>
- 
- 
-       <br>
-       <br>
- 
-       <button @click="gender2()" id="girl" value="girl">
-         <img src="@/assets/girl.png" alt="" />
-       </button>
- 
- 
-     </div>
-   </div>
- </div>
+<template>
+  <div class="container">
+    <h2>Step (1/4)</h2>
+    <div>
+      <h3>What is your gender?</h3>
+      <div class="gender-box">
+        <div @click="setGender('Boy')" id="boy" value="boy">
+          <img src="@/assets/boy.png" alt="" />
+          <span>male</span>
+        </div>
+        <br />
+        <div @click="setGender('Girl')" id="girl" value="girl">
+          <img src="@/assets/girl.png" alt="" />
+          <span>Female</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
- 
- 
-
 
 <script>
 import firebaseApp from '../firebase.js';
@@ -45,23 +38,23 @@ export default {
     });
   },
   methods: {
-    async gender1() {
+    async setGender(gender) {
       try {
-        console.log('its a boy!');
-        await setDoc(doc(db, 'profile', 'gender'), {
-          gender: 'Boy',
-        });
-        this.$router.push('./goalStep2');
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async gender2() {
-      try {
-        console.log('its a girl!');
-        await setDoc(doc(db, 'profile', 'gender'), {
-          gender: 'Girl',
-        });
+        if(gender === 'Boy'){
+          await setDoc(doc(db, 'profile', 'gender'), {
+            gender: 'Boy',
+          });
+        } else if( gender === 'Girl') {
+          await setDoc(doc(db, 'profile', 'gender'), {
+            gender: 'Girl',
+          });
+        } else {
+          alert('Something went wrong, please try again')
+          return false;
+        }
+
+        this.$store.commit('setGender', gender);
+        
         this.$router.push('./goalStep2');
       } catch (error) {
         console.log(error);
@@ -71,119 +64,33 @@ export default {
 };
 </script>
 
+<style scoped>
+.gender-box{
+  display: flex;
+  justify-content: space-between;
  
-<style type="text/css">
-/*
-Color Theme
-006d77
-83c5be
-edf6f9
-ffddd2
-e29578
-*/
- 
-@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
- 
-body {
- box-sizing: border-box;
- margin: 0;
- padding: 0;
- background-color: white;
- font-family: "lato", sans-serif;
- text-align: center;
+  max-width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 40px;
 }
- 
- 
-img { 
-max-width: 100%; 
-height: auto; 
-} 
- 
- 
- 
- 
-.form {
- display:inline-block;
- background-color: white;
- width: 500px;
- border-radius: 8px;
- padding: 20px 40px;
- box-shadow: 0 10px 25px rgba(92, 99, 105, .2);
- align-items: center;
-}
- 
- 
-.title {
- font-size: 50px;
- margin-bottom: 100px;
-}
- 
-/* Hide the placeholder texts (a) */
-::placeholder {
- color: transparent;
-}
- 
- 
-.input {
- position: relative;
- top: 0px;
- left: 0px;
- height: 100%;
- width: 100%;
- border: 3px solid #DADCE0;
- border-radius: 3px;
- font-size: 18px;
- outline: none;
- background: none;
- z-index: 1;
-}
- 
-.input:focus {
- border: 3px solid#83c5be;
-}
- 
- 
-.label {
- position: absolute;
- top: 15px;
- left: 15px;
- padding: 0 4px;
- background-color: white;
- color: #006d77;
- font-size: 16px;
- transition: 0.5s;
- z-index: 0;
-}
- 
-#boy{
- border-radius: 40%;
- width: 250px;
-}
- 
-#girl{
- border-radius: 40%;
- width: 250px;
-}
- 
-.Btn {
- display: block;
- margin-left: auto;
- padding: 15px 30px;
- border: none;
- background-color: #e29578;
- color: white;
- border-radius: 6px;
- cursor: pointer;
- font-size: 16px;
- margin-top: 30px;
-}
+.gender-box div{
+  max-width: 100%;
+  background: transparent;
+  border:none;
+  width: 150px;
+  cursor: pointer;
+  height: 200px;
 
-.Btn:hover {
- background-color: #f1c9bb;
- transform: translateY(-2px);
 }
- 
- 
-
- 
+.gender-box div img{
+  object-fit: cover;
+  display: block;
+  height: 100%;
+  width: 100%;
+}
+.gender-box div span{
+  font-size: 20px;
+  font-weight: 600;
+}
 </style>
--- src/components/CreateGoal2.vue
+++ src/components/CreateGoal2.vue
@@ -1,29 +1,21 @@
 <template>
- <div class="container">
- 
-   <h2 class = "Title">Step (2/4)</h2>
-   <h3>What is your height?</h3>
- 
-   <div id="flex-container">
- 
-     <div id="flex-child">
-       <img src="@/assets/height.png" alt="" class="" />
-     </div>
- 
-        
-     <form id="flex-child" class="form">       
-       <input class = "input" type="text" placeholder = "ph" id="height" />
-       <label for="height">My height (cm) is</label>
-     </form>
-    
- 
-     <button class ="Btn" @click="step2()">Next</button>
-   </div>
- </div>
+  <div class="container">
+  <h2>Step (2/4)</h2>
+  <h3>What is your height?</h3>
+  <div class="mx-600">
+    <div class="left">
+      <div class="img-box">
+        <img src="@/assets/height.png" alt="" class="" />
+      </div>
+    </div>
+    <form class="right">
+      <label for="height">My height (cm) is</label>
+      <input type="text" v-model="height" id="height" />
+      <button @click.prevent="step2()">Next</button>
+    </form>
+    </div>
+  </div>
 </template>
-
-
-
 
 <script>
 import firebaseApp from '../firebase.js';
@@ -35,6 +27,11 @@ const db = getFirestore(firebaseApp);
 
 export default {
   components: {},
+  data (){
+    return {
+      height: 0,
+    }
+  },
   mounted() {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
@@ -46,127 +43,88 @@ export default {
   methods: {
     async step2() {
       try {
-        var height = document.getElementById('height').value;
+        let height = this.height;
+
+        if (height < 30) {
+          alert('Please enter a correct height in CM')
+          return false;
+        }
+
         await setDoc(doc(db, 'profile', 'height'), {
           height: height,
         });
-        console.log('pushing?');
+
+        this.$store.commit('setHeight', height);
         this.$router.push('./goalStep3');
-        console.log('pushed');
       } catch (error) {
         console.log(error);
       }
     },
   },
+  created(){
+
+    if(!this.$store.getters.getGoal.gender){
+      this.$router.push('./goalStep1');
+    }
+  }
 };
 </script>
 
-
- 
-<style type="text/css">
-/*
-Color Theme
-006d77
-83c5be
-edf6f9
-ffddd2
-e29578
-*/
- 
-@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
-
-body {
- box-sizing: border-box;
- margin: 0;
- padding: 0;
- background-color: white;
- font-family: "lato", sans-serif;
- text-align: center;
+<style  lang="scss">
+.mx-600{
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
+  display: flex;
+  align-items: center;
+  button{
+    width: 100%;
+    max-width: 100px;
+    padding: 7px 10px;
+    border: 1px solid #2C3D50;
+    cursor: pointer;
+    margin-top: 15px;
+  }
+}
+.left{
+  .img-box{
+    width: 350px;
+    height: 300px;
+    img{
+      display: block;
+      width: 100%;
+      position: relative;
+      top: -30px;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+.right{
+  position: relative;
+  left: -100px;
+  top: -30px;
+  label{
+    display: block;
+    margin: 0px;
+    font-size: 16px;
+    font-weight: 600;
+    margin-left: -20px;
+  }
+  input{
+    max-width: 200px;
+    display: block;
+    border: none;
+    background: #D9D9D9;
+    height: 35px;
+    padding-left: 15px;
+    margin-top: 10px;
+    border-radius: 2px;
+    &:focus{
+    outline: none;
+    border: none;
+    }
+  }
 }
 
- 
- 
-img { 
-max-width: 100%; 
-height: auto; 
-} 
- 
- 
- 
- 
-.form {
- display:inline-block;
- background-color: white;
- width: 500px;
- border-radius: 8px;
- padding: 20px 40px;
- box-shadow: 0 10px 25px rgba(92, 99, 105, .2);
- align-items: center;
-}
- 
- 
-.title {
- font-size: 50px;
- margin-bottom: 100px;
-}
- 
-/* Hide the placeholder texts (a) */
-::placeholder {
- color: transparent;
-}
- 
- 
-.input {
- position: relative;
- top: 0px;
- left: 0px;
- height: 100%;
- width: 100%;
- border: 3px solid #DADCE0;
- border-radius: 3px;
- font-size: 18px;
- outline: none;
- background: none;
- z-index: 1;
-}
- 
-.input:focus {
- border: 3px solid#83c5be;
-}
- 
- 
-.label {
- position: absolute;
- top: 15px;
- left: 15px;
- padding: 0 4px;
- background-color: white;
- color: #006d77;
- font-size: 16px;
- transition: 0.5s;
- z-index: 0;
-}
- 
- 
-.Btn {
- display: block;
- margin-left: auto;
- padding: 15px 30px;
- border: none;
- background-color: #e29578;
- color: white;
- border-radius: 6px;
- cursor: pointer;
- font-size: 16px;
- margin-top: 30px;
-}
- 
-.Btn:hover {
- background-color: #f1c9bb;
- transform: translateY(-2px);
-}
- 
- 
- 
- 
 </style>

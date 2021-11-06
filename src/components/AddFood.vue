12bf7d0f8a52b1fe53d8c58f2a6d785c8ac3f0c6
pngqunshen <pngqunshen@gmail.com>
2021-11-07 03:08:27 +0800

    added multiple add food feature

--- src/components/AddFood.vue
+++ src/components/AddFood.vue
@@ -2,7 +2,7 @@
   <div id="addFood">
     <h3>{{ mealType }}</h3>
 
-    <div id="mealInfo" v-if="displayFoodInfo" :key="refreshCounter">
+    <div id="mealInfo" v-show="displayFoodInfo">
       <div style="width: 20%">
         <img id="foodImageID" v-bind:src="imageSource" style="mealImageStyle" />
         <input
@@ -21,88 +21,17 @@
         </button>
       </div>
       <div id="mealNutrient">
-      
-      <table>
-        <tr>
-        <td>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Food Name</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="item in mealName" :key="item">
-                      <td>{{ item }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </td>
+        <table class="mealTable" :id="mealTableID">
+          <tr>
+            <th>Food Name</th>
+            <th>Calorie</th>
+            <th>Fat</th>
+            <th>Protein</th>
+            <th>Carbohydrates</th>
+            <th>Options</th>
+          </tr>
+        </table>
 
-        <td>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Calories</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="item in mealCal" :key="item">
-                      <td>{{ item }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </td>
-
-        <td>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Fat</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="item in mealFat" :key="item">
-                      <td>{{ item }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </td>
-
-        <td>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Protein</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="item in mealProtein" :key="item">
-                      <td>{{ item }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </td>
-
-        <td>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Carbohydrates</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="item in mealCarb" :key="item">
-                      <td>{{ item }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </td>
-        
-        </tr>
-      </table>
-             
-        
         <br /><br />
 
         <button type="button" id="addFoodButton" v-on:click="deleteMeal">
@@ -112,15 +41,18 @@
     </div>
 
     <button
-      v-if="!displayTable"
+      v-show="!displayTable"
       type="button"
       id="addFoodButton"
       v-on:click="displayCalc()"
     >
       Add Food
     </button>
-    <div v-if="displayTable">
-      <APIQuery @chosenFood="foodChosen($event)" :foodTableID="mealType"/><br /><br />
+    <div v-show="displayTable">
+      <APIQuery
+        @chosenFood="foodChosen($event)"
+        :foodTableID="mealType"
+      /><br /><br />
       <div v-if="recipe">Current food selected: {{ recipe["label"] }}</div>
       <div v-else>Select a food!</div>
       <br /><br />
@@ -135,9 +67,15 @@
 
 <script>
   import firebaseApp from "../firebase.js";
-  import { getDoc, getFirestore, setDoc, updateDoc, deleteDoc } from "firebase/firestore";
+  import {
+    getDoc,
+    getFirestore,
+    setDoc,
+    deleteDoc,
+    updateDoc,
+    deleteField,
+  } from "firebase/firestore";
   import { doc } from "firebase/firestore";
-
   import { getAuth, onAuthStateChanged } from "firebase/auth";
   import APIQuery from "./APIQuery.vue";
   import {
@@ -148,9 +86,7 @@
     deleteObject,
   } from "firebase/storage";
   import no_image_loaded from "@/assets/no_image_uploaded.png";
-
   const db = getFirestore(firebaseApp);
-
   export default {
     data() {
       return {
@@ -160,11 +96,7 @@
         currUploadedImage: null,
         haveImage: false,
         imageSource: no_image_loaded,
-        mealName: new Array(),
-        mealProtein: new Array(),
-        mealCarb: new Array(),
-        mealFat: new Array(),
-        mealCal: new Array(),
+        mealDetail: {},
         refreshCounter: 0,
       };
     },
@@ -174,6 +106,9 @@
           getStorage(),
           this.fbuser + "/" + this.mealDate + "/" + this.mealType
         );
+      },
+      mealTableID() {
+        return "mealTable" + this.mealType;
       },
     },
     mounted() {
@@ -238,68 +173,32 @@
           return;
         }
         try {
-          let a = doc(
-            doc(db, String(this.fbuser), "daily_nutrient"),
-            this.mealDate,
-            this.mealType
+          setDoc(
+            doc(
+              doc(db, String(this.fbuser), "daily_nutrient"),
+              this.mealDate,
+              this.mealType
+            ),
+            {
+              [this.recipe["label"]]: {
+                calorie: Math.round(
+                  this.recipe["calories"] / this.recipe["yield"]
+                ),
+                fat: Math.round(
+                  this.recipe["totalNutrients"]["FAT"]["quantity"] /
+                    this.recipe["yield"]
+                ),
+                protein: Math.round(
+                  this.recipe["totalNutrients"]["PROCNT"]["quantity"] /
+                    this.recipe["yield"]
+                ),
+                carbohydrates: Math.round(
+                  this.recipe["totalNutrients"]["CHOCDF"]["quantity"] /
+                    this.recipe["yield"]
+                ),
+              },
+            }, { merge: true}
           );
-          let b = await getDoc(a);
-          let c = b.data();
-          if (c == undefined){
-            setDoc(
-            doc(
-              doc(db, String(this.fbuser), "daily_nutrient"),
-              this.mealDate,
-              this.mealType
-            ),
-            {
-              [this.recipe["label"]]: {
-                calorie: Math.round(
-                  this.recipe["calories"] / this.recipe["yield"]
-                ),
-                fat: Math.round(
-                  this.recipe["totalNutrients"]["FAT"]["quantity"] /
-                    this.recipe["yield"]
-                ),
-                protein: Math.round(
-                  this.recipe["totalNutrients"]["PROCNT"]["quantity"] /
-                    this.recipe["yield"]
-                ),
-                carbohydrates: Math.round(
-                  this.recipe["totalNutrients"]["CHOCDF"]["quantity"] /
-                    this.recipe["yield"]
-                ),
-              },
-            }
-            );
-          }else{
-            updateDoc(
-            doc(
-              doc(db, String(this.fbuser), "daily_nutrient"),
-              this.mealDate,
-              this.mealType
-            ),
-            {
-              [this.recipe["label"]]: {
-                calorie: Math.round(
-                  this.recipe["calories"] / this.recipe["yield"]
-                ),
-                fat: Math.round(
-                  this.recipe["totalNutrients"]["FAT"]["quantity"] /
-                    this.recipe["yield"]
-                ),
-                protein: Math.round(
-                  this.recipe["totalNutrients"]["PROCNT"]["quantity"] /
-                    this.recipe["yield"]
-                ),
-                carbohydrates: Math.round(
-                  this.recipe["totalNutrients"]["CHOCDF"]["quantity"] /
-                    this.recipe["yield"]
-                ),
-              },
-            }
-            );
-          }
         } catch (error) {
           console.error("Error adding document: ", error);
         }
@@ -317,41 +216,48 @@
           let b = await getDoc(a);
           let c = b.data();
           if (c == undefined) return;
-          var i = 0;
+          this.mealDetail = {};
           Object.entries(c).forEach((entry) => {
-            this.mealName[i] = entry[0];
-            this.mealProtein[i] = entry[1].protein;
-            this.mealCarb[i] = entry[1].carbohydrates;
-            this.mealFat[i] = entry[1].fat;
-            this.mealCal[i] = entry[1].calorie;
-
-    /*        var table = document.getElementById("mealTable")
-            var row = table.insertRow(i+1)
-
-            var cell1 = row.insertCell(0); 
-            var cell2 = row.insertCell(1); 
-            var cell3 = row.insertCell(2);
-            var cell4 = row.insertCell(3); 
-            var cell5 = row.insertCell(4); 
-            
-
-            cell1.innerHTML = this.mealName[i]; 
-            cell2.innerHTML = this.mealProtein[i]; 
-            cell3.innerHTML = this.mealCarb[i]; 
-            cell4.innerHTML = this.mealFat[i]; 
-            cell5.innerHTML = this.mealFat[i];*/
-            i++;
-     
-            
+            this.mealDetail[entry[0]] = {
+              protein: entry[1].protein,
+              carbohydrates: entry[1].carbohydrates,
+              fat: entry[1].fat,
+              calorie: entry[1].calorie,
+            };
           });
           this.displayFoodInfo = true;
+          this.updateMealTable();
           this.loadImage();
         } catch (error) {
           if (error.code == "invalid-argument") return;
           console.error("Error getting document: ", error.code);
         }
       },
-      
+      updateMealTable() {
+        for (
+          var i = document.getElementById(this.mealTableID).rows.length;
+          i > 1;
+          i--
+        ) {
+          document.getElementById(this.mealTableID).deleteRow(i - 1);
+        }
+        var ind = 1;
+        Object.entries(this.mealDetail).forEach((entry) => {
+          var row = document.getElementById(this.mealTableID).insertRow(ind);
+          row.insertCell(0).innerHTML = entry[0];
+          row.insertCell(1).innerHTML = entry[1]["calorie"];
+          row.insertCell(2).innerHTML = entry[1]["fat"];
+          row.insertCell(3).innerHTML = entry[1]["protein"];
+          row.insertCell(4).innerHTML = entry[1]["carbohydrates"];
+          let bu = document.createElement("button");
+          bu.id = "deleteFoodButton";
+          bu.type = "button";
+          bu.innerHTML = "Delete";
+          bu.onclick = () => this.deleteFood(entry[0]);
+          row.insertCell(5).appendChild(bu);
+        });
+        this.refreshCounter++;
+      },
       async deleteMeal() {
         alert("You are going to delete " + this.mealType);
         await deleteDoc(
@@ -372,7 +278,19 @@
         this.displayFoodInfo = false;
         this.imageSource = no_image_loaded;
         this.haveImage = false;
-        this.recipe = null;
+      },
+      async deleteFood(foodName) {
+        updateDoc(
+          doc(
+            doc(db, String(this.fbuser), "daily_nutrient"),
+            this.mealDate,
+            this.mealType
+          ),
+          {
+            [foodName]: deleteField(),
+          }
+        );
+        this.getFoodData();
       },
     },
   };
@@ -385,30 +303,29 @@
     margin-right: 10%;
     text-align: left;
   }
-
   #mealInfo {
     display: flex;
     width: 100%;
     text-align: center;
   }
-
   #mealNutrient {
     width: 80%;
     height: 100%;
-    margin-top: 75px;
+    /* margin-top: 75px; */
   }
-
   #foodImageID {
     width: 100%;
   }
-
   #addFoodButton {
     width: 15%;
     height: 30px;
     border-radius: 5px;
   }
-
-  #mealTable {
+  #deleteFoodButton {
+    width: 80%;
+    border-radius: 5px;
+  }
+  .mealTable {
     font-family: arial, sans-serif;
     border-collapse: collapse;
     width: 90%;
@@ -417,12 +334,10 @@
     margin-left: 5%;
     margin-right: 5%;
   }
-
-  #mealTable th {
+  .mealTable th {
     border: 3px solid black;
     text-align: center;
     background-color: #575454;
     color: white;
   }
-  
 </style>
